b803953fd341e1d287cc0e35d9b79cb97d72bcea
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-04-16 16:25:25 +0800

    added components

---
+++ components/Container.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+interface Props {
+  children: ReactNode
+}
+
+export const Container = ({ children }: Props) => {
+  return <div className="container">{children}</div>
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import {AppProps} from 'next/app'
+import { AppProps } from 'next/app'
+import Layout from '../components/Layout'
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
