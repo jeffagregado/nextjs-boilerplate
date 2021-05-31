05c858bbca42ce216e8b09f1a2d3e2652a55db24
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-31 14:49:02 +0800

    troubleshooting, fixing errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,15 +2,57 @@ import { ReactNode } from 'react'
 import setClassNames from 'classnames'
 import Image from 'next/image'
 import { Container } from './Container'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useCycle } from 'framer-motion'
 
 interface Props {
   children: ReactNode
   className?: string
-  src: string
-  alt: string
 }
 
-const Hero = ({ children, className, src, alt }: Props) => {
+const Hero = ({ children, className }: Props) => {
+  const [photos, setPhotos] = useState([''])
+
+  const fetchPhotos = async () => {
+    const photoUrl = 'https://picsum.photos/v2/list?limit=10'
+
+    await axios
+      .get(photoUrl)
+      .then((res) => {
+        return res
+      })
+      .then((data) => {
+        const dataSet = data.data
+        console.log(dataSet)
+        setPhotos(dataSet)
+      })
+  }
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+  // cycle between photos
+  const photosUrl = photos.map((photo: any) => photo.download_url)
+  //const photosAuthor = photos.map((photo: any) => photo.author)
+
+  const [currentScene, setCurrentScene] = useCycle(
+    photosUrl[0],
+    photosUrl[0 + 1]
+  )
+
+  useEffect(() => {
+    const timeOut = setTimeout(setCurrentScene, 5000)
+    return () => {
+      clearTimeout(timeOut)
+    }
+  }, [currentScene, setCurrentScene])
+
+  //const photoAuthor = currentScene.map(photo => photo.author)
+
+  console.log('url', photosUrl[0])
+
   return (
     <header
       className={setClassNames(
@@ -18,7 +60,8 @@ const Hero = ({ children, className, src, alt }: Props) => {
         className && className
       )}
     >
-      <Image src={src} alt={alt} layout="fill" />
+      {/* <Image src={photosUrl[0]} alt='carousel' layout="fill" /> */}
+      {/* <Carousel src={currentScene ? currentScene : photosUrl[0]} alt='carousel' /> */}
       <Container>
         <div>{children}</div>
       </Container>
--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
-const path = require('path')
-
-module.exports = (phase, { defaultConfig }) => {
-  if ('sassOptions' in defaultConfig) {
-    defaultConfig['sassOptions'] = {
-      //includePaths: ['./styles/'],
-      includePaths: [path.join(__dirname, 'styles')],
-      prependData: `@use 'utilities' as *;`,
-    }
-  }
-  return defaultConfig
-}
-
 module.exports = {
-  future: {
-    webpack5: true,
+  images: {
+    domains: ['picsum.photos'],
   },
 }
