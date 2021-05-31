f5544286fbb19d04dd6cc6e3330a491cad6031ef
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-31 18:05:32 +0800

    align item center lists

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -3,7 +3,7 @@ import NavLinks from './NavLinks'
 
 const Nav = () => {
   return (
-    <nav className="container flex my-4 justify-between">
+    <nav className="container flex my-4 justify-between items-center">
       <Logo className="flex-1" />
       <NavLinks />
     </nav>
