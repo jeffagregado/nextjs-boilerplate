80e8957ae6755fa6eaad64fdba29ba9cbbb5720b
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-09-25 23:10:43 +0800

    change nav style

--- components/Nav/NavLinksV2.tsx
+++ components/Nav/NavLinksV2.tsx
@@ -9,17 +9,17 @@ const NavLinksV2 = ({ toggle }: Props) => {
   return (
     <ul
       className={classnames(
-        `text-white grid grid-cols-1 col-start-1 col-end-4 bg-gray-500 text-center overflow-hidden transition-all ease-in-out duration-150 md:flex md:gap-x-8 md:m-0 md:px-4 md:col-start-3 md:max-h-10 md:bg-black`,
+        `text-black md:text-white grid grid-cols-1 col-start-1 col-end-4 bg-gray-400 text-center overflow-hidden transition-all ease-in-out duration-150 md:flex md:gap-x-8 md:m-0 md:px-4 md:col-start-3 md:max-h-10 md:bg-black`,
         `${toggle ? `max-h-[400px]` : `max-h-0`}`
       )}
     >
-      <li className="py-2 md:p-0">
+      <li className="py-3 md:p-0 border-b-2 border-b-black md:border-none">
         <Link url="#">Link 1</Link>
       </li>
-      <li className="py-2 md:p-0">
+      <li className="py-3 md:p-0 border-b-2 border-b-black md:border-none">
         <Link url="#">Link 2</Link>
       </li>
-      <li className="py-2 md:p-0">
+      <li className="py-3 md:p-0">
         <Link url="#">Link 3</Link>
       </li>
     </ul>
