80aaf2ddf9697121a1107fd54109735beff8b366
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-10-27 16:54:12 +0800

    fixed NavV2 and added debugger config

--- components/Nav/NavV2.tsx
+++ components/Nav/NavV2.tsx
@@ -6,8 +6,8 @@ import NavLinksV2 from './NavLinksV2'
 const NavV2 = () => {
   const [toggle, setToggle] = useState(false)
   return (
-    <header className="fixed top-0 z-10 w-full bg-black">
-      <div className="container max-w-none md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1536px] px-0 md:p-4">
+    <header className="w-full bg-black">
+      <div className="container px-0 md:p-4">
         <nav className="grid grid-cols-2 items-center">
           <Logo className="text-white mx-8 md:mx-0" />
           {/* <label
